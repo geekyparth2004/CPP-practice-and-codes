58088d8d4b36d92d3e4a5b6ff2c2640e41a82aa2
geekyparth2004 <goelparth20049@gmail.com>
2025-01-06 12:32:42 +0530

    Leetcode question

---
+++ c++/leetcode/242.valid-anagram.cpp
@@ -0,0 +1,61 @@
+/*
+ * @lc app=leetcode id=242 lang=cpp
+ *
+ * [242] Valid Anagram
+ *
+ * https://leetcode.com/problems/valid-anagram/description/
+ *
+ * algorithms
+ * Easy (65.51%)
+ * Likes:    12600
+ * Dislikes: 418
+ * Total Accepted:    4.3M
+ * Total Submissions: 6.5M
+ * Testcase Example:  '"anagram"\n"nagaram"'
+ *
+ * Given two strings s and t, return true if t is an anagram of s, and false
+ * otherwise.
+ * 
+ * 
+ * Example 1:
+ * 
+ * 
+ * Input: s = "anagram", t = "nagaram"
+ * 
+ * Output: true
+ * 
+ * 
+ * Example 2:
+ * 
+ * 
+ * Input: s = "rat", t = "car"
+ * 
+ * Output: false
+ * 
+ * 
+ * 
+ * Constraints:
+ * 
+ * 
+ * 1 <= s.length, t.length <= 5 * 10^4
+ * s and t consist of lowercase English letters.
+ * 
+ * 
+ * 
+ * Follow up: What if the inputs contain Unicode characters? How would you
+ * adapt your solution to such a case?
+ * 
+ */
+
+// @lc code=start
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        sort(s.begin(),s.end());
+        sort(t.begin(),t.end());
+
+        return s==t; 
+    }
+};
+// @lc code=end
+
